b5ee8cd9ce687ef74bb3c587a5cfdd9e0c373db0
EnekoAzkue <eneko.azkue@ikasle.aeg.eus>
2025-09-25 13:26:08 +0200

    feat(CEJ-18)  add link to the Mongo database

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose')
-const mongodbRoute = 'mongo string conection goes here'
+const mongodbRoute = 'mongodb+srv://enekoazkue:enekoazkuepasswd@cluster0.ylpw1pf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 
 const app = express();
 
