592d268215d57a870af2401db8bcfb248cc23eec
Carlos <carlos012100@gmail.com>
2025-09-25 19:02:24 +0200

    fix[CEJ-21]:build the player schema with all its attributes/fields, making use of subSchemas,nested obj, spread

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -22,28 +22,88 @@ const attributesSchema = new Schema({
 
 //but first define basic item fields
 
-const itemFields = new Schema ({
+const modifiersSub = {
+
+  intelligence: Number,
+  dexterity: Number,
+  constitution: Number,
+  insanity: Number,
+  charisma: Number,
+  strength: Number
+
+};
+const itemFields = ({
+
     name: String,
-    description: String
-})
+    description: String,
+    type: String,
+    image: String,
+    value: Number,
+    min_lvl: Number,
+    profiles: [String]
+
+});
 
 const weaponsSchema = new Schema({
-    name: String,
-    description: String
+    modifiers: modifiersSub,
+    ...itemFields,
+    base_percentage: Number,
+    die_faces: Number,
+    die_modifier: Number,
+    die_num: Number
 });
 
 const artifactsSchema = new Schema({
-    name: String,
-    description: String
+    modifiers: modifiersSub,
+    ...itemFields
+    
+});
+const armorsSchema = new Schema({
+    modifiers: modifiersSub,
+    ...itemFields,
+    defense: Number
+
 })
+
+const healingPotSchema = new Schema ({
+    ...itemFields,
+
+})
+
+
+const recoveryEffectsSchema = new Schema({
+    modifiers: modifiersSub,
+    name: String,
+    description: String,
+    type: String,
+    antidote_effects: [String],
+    poison_effects: [String]
+
+
+})
+
+const antidoteSchema = new Schema({
+    ...itemFields,
+    recover_effect: recoveryEffectsSchema
+
+
+
+});
+
+const enhancerPotSchema = new Schema ({
+    modifiers: modifiersSub,
+    ...itemFields,
+    duration: Number
+})
+
 const equipmentSchema = new Schema({
 
     weapons: [weaponsSchema],    
     artifacts: [artifactsSchema],
-    armors:[],
-    healing_potions: [],
-    antidote_potions: [],
-    enhancer_potions: []
+    armors:[armorsSchema],
+    healing_potions: [healingPotSchema],
+    antidote_potions: [antidoteSchema],
+    enhancer_potions: [enhancerPotSchema]
 
 });
 
