206935373a61b8a042facc67b3488bd8bb7eb704
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2025-09-26 08:34:29 +0200

    feat(cej-26): load the userRouter in the app

--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
-const express = require("express")
+const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require('mongoose')
-const mongodbRoute = 'mongo string conection goes here'
+const userRouter = require("./routes/userRoutes");
+const mongoose = require("mongoose");
+
+const mongodbRoute = "mongo string conection goes here";
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use("/user", userRouter);
 
-async function start(){
-    try
-    {
-        await mongoose.connect(mongodbRoute);
-        app.listen(PORT, () => {
-            console.log(`API is listening on port ${PORT}`);
-        });
-        console.log('you are now connected to Mongo')
-    } 
-    catch(error)
-    {
-        console.log(`Error to connect to the database: ${error.message}`);
-    }
+async function start() {
+  try {
+    await mongoose.connect(mongodbRoute);
+
+    app.listen(PORT, () => {
+      console.log(`API is listening on port ${PORT}`);
+    });
+
+    console.log("you are now connected to Mongo");
+  } catch (error) {
+    console.log(`Error to connect to the database: ${error.message}`);
+  }
 }
-start ();
+
+start();
