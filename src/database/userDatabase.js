493350ef83e39c0a760ae541c44a53a5ff182055
EnekoAzkue <eneko.azkue@ikasle.aeg.eus>
2025-09-27 15:46:48 +0200

    feat(CEJ-35) complete the database layer with the needed functions

--- src/database/userDatabase.js
+++ src/database/userDatabase.js
@@ -1,9 +1,13 @@
-const userDatabase = require('.../models/userModel');
+const userModel = require('.../models/userModel');
 
-const getAllUsers = async () => {
+//UNCOMMENT AND REPLACE WHEN MIDDLEWARE IS IMPLEMENTED
+// const getUser = async (res.locals.userEmail) => {
+const getUser = async (userEmail) => {
     try
     {
-        const users = await userDatabase.find()
+        //UNCOMMENT AND REPLACE WHEN MIDDLEWARE IS IMPLEMENTED
+        // const users = await userModel.findByOne(res.locals.userEmail);
+        const users = await userModel.findByOne(userEmail);
         return users;
     }
     catch (error)
@@ -12,6 +16,38 @@ const getAllUsers = async () => {
     }
 };
 
+const createUser = async (newUser) => {
+    try
+    {
+        let userToInsert = new userModel(newUser);
+        const createduser = await userToInsert.save( );
+        return createduser;
+    }
+    catch (error)
+    {
+        throw error;
+    }
+};
+
+    //UNCOMMENT AND REPLACE WHEN MIDDLEWARE IS IMPLEMENTED
+    // const updateUser = async(res.locals.userEmail, changes) => {
+    const updateUser = async(userEmail, changes) => {
+    try
+    {
+        //UNCOMMENT AND REPLACE WHEN MIDDLEWARE IS IMPLEMENTED
+        // let updatedUser = await userModel.findByOnedAndUpdate(res.locals.userEmail,
+        let updatedUser = await userModel.findByOneAndUpdate(userEmail,
+{ $set: changes }, { new: true });
+        return updatedUser;
+    }
+    catch (error)
+    {
+        throw error;
+    }
+};
+
 module.exports = {
-    getAllUsers
+    getUser,
+    createUser,
+    updateUser
 }
