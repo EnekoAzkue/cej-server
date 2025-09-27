5242e30bba9a412a28cd5ad3c8b4111beba3d5b4
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2025-09-27 12:57:14 +0200

    feat(cej-34): protect confidential data using environment variables

--- src/index.js
+++ src/index.js
@@ -2,15 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const userRouter = require("./routes/userRoutes");
 const mongoose = require("mongoose");
-const admin = require("firebase-admin");
-const serviceAccount = require("../er6-cej-firebase-adminsdk-fbsvc-8da2344087.json");
+const { initializeApp, applicationDefault } = require("firebase-admin/app");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+initializeApp({
+  credential: applicationDefault(),
 });
-
-const mongodbRoute =
-  "mongodb+srv://enekoazkue:enekoazkuepasswd@cluster0.ylpw1pf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -20,13 +16,13 @@ app.use("/user", userRouter);
 
 async function start() {
   try {
-    await mongoose.connect(mongodbRoute);
+    await mongoose.connect(process.env.MONGODB_ROUTE);
 
     app.listen(PORT, () => {
-      console.log(`API is listening on port ${PORT}`);
+      console.log(`API is listening on port ${PORT}.`);
     });
 
-    console.log("you are now connected to Mongo");
+    console.log("You are now connected to Mongo.");
   } catch (error) {
     console.log(`Error to connect to the database: ${error.message}`);
   }
