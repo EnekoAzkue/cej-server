5753c2344f393ca77a970380265977531f0b043b
EnekoAzkue <eneko.azkue@ikasle.aeg.eus>
2025-09-28 23:26:29 +0200

    feat(CEJ-24) implement login service

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,65 +1,78 @@
 const User = require('../database/userDatabase');
 
-//UNCOMMENT AND REPLACE WHEN MIDDLEWARE IS IMPLEMENTED
-// const getUser = async (res.locals.userEmail) => {
 const getUser = async (userEmail) => {
-    try
-    {
-        //UNCOMMENT AND REPLACE WHEN MIDDLEWARE IS IMPLEMENTED
-        // const users = await User.getUser(res.locals.userEmail);
-        const users = await User.getUser(userEmail);
-        return users;
-    }
-    catch (error)
-    {
-        throw error;
-    }
+  try {
+    const user = await User.getUser(userEmail);
+    console.log('Fetched user from MongoDB:', userEmail);
+    return user;
+  } catch (error) {
+    throw error;
+  }
 };
 
-//UNCOMMENT AND REPLACE WHEN MIDDLEWARE IS IMPLEMENTED
-// const getKaotikaUser = async (res.locals.userEmail) => {
 const getKaotikaUser = async (userEmail) => {
-    try {
-        // const response = await fetch(`https://kaotika-server.fly.dev/players/email/${res.locals.userEmail}`);
-        const response = await fetch(`https://kaotika-server.fly.dev/players/email/${userEmail}`);
-        const kaotikaUser = await response.json();
-        return kaotikaUser;
-    } catch (error) {
-        throw error;
+  try {
+    console.log(`Fetching user from Kaotika with email: ${userEmail}`);
+    const response = await fetch(`https://kaotika-server.fly.dev/players/email/${userEmail}`);
+    if (!response.ok) {
+      throw new Error(`Kaotika API error: ${response.status}`);
     }
+    const kaotikaUser = await response.json();
+    return kaotikaUser || null;
+  } catch (error) {
+    throw error;
+  }
 };
 
 const createUser = async (newUser) => {
-    try
-    {
-        const createdUser = await User.createUser(newUser);
-        return createdUser;
-    }
-    catch (error)
-    {
-        throw error;
-    }
-}
+  try {
+      
+      const createdUser = await User.createUser(newUser);
+      console.log('Creating new user in MongoDB');
+    return createdUser;
+  } catch (error) {
+    throw error;
+  }
+};
 
-    //UNCOMMENT AND REPLACE WHEN MIDDLEWARE IS IMPLEMENTED
-    // const updateUser = async(res.locals.userEmail, changes) => {
-    const updateUser = async(userEmail, changes) => {
-    try
-    {
-        //UNCOMMENT AND REPLACE WHEN MIDDLEWARE IS IMPLEMENTED
-        // const updatedUser = await User.updateUser(res.locals.userEmail, changes);
-        const updatedUser = await User.updateUser(userEmail, changes);
-        return updatedUser;
+const updateUser = async (userEmail, changes) => {
+  try {
+    const updatedUser = await User.updateUser(userEmail, changes);
+    return updatedUser;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const loginUser = async (userEmail) => {
+  try {
+    const kaotikaUser = await getKaotikaUser(userEmail);
+    console.log('Kaotika user data:', kaotikaUser);
+    if (!kaotikaUser) {
+      throw new Error(`User not found in Kaotika with email: ${userEmail}`);
     }
-    catch (error)
-    {
-        throw error;
+
+    const mongoUser = await getUser(userEmail);
+
+    if (!mongoUser) {
+      const newUser = {
+        ...kaotikaUser,   
+        active: false,    
+      };
+      return await createUser(kaotikaUser);
     }
+
+    return await updateUser(userEmail, { active: true });
+
+  } catch (error) {
+    throw error;
+  }
 };
 
 module.exports = { 
-    getUser,
-    createUser,
-    updateUser,
-    getKaotikaUser
-}
+  getUser,
+  createUser,
+  updateUser,
+  getKaotikaUser,
+  loginUser
+};
