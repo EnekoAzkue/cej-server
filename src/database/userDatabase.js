0c26222ae6bfb3d0c2fd5c44c2253539b15ccbb7
EnekoAzkue <eneko.azkue@ikasle.aeg.eus>
2025-09-28 23:30:56 +0200

    fix(CEJ-25) fix the data access layer

--- src/database/userDatabase.js
+++ src/database/userDatabase.js
@@ -1,53 +1,37 @@
-const userModel = require('.../models/userModel');
+const userModel = require('../models/userModel');
 
-//UNCOMMENT AND REPLACE WHEN MIDDLEWARE IS IMPLEMENTED
-// const getUser = async (res.locals.userEmail) => {
 const getUser = async (userEmail) => {
-    try
-    {
-        //UNCOMMENT AND REPLACE WHEN MIDDLEWARE IS IMPLEMENTED
-        // const users = await userModel.findByOne(res.locals.userEmail);
-        const users = await userModel.findByOne(userEmail);
-        return users;
-    }
-    catch (error)
-    {
-        throw error;
-    }
+  try {
+    const user = await userModel.findOne({ email: userEmail });
+    return user;
+  } catch (error) {
+    throw error;
+  }
 };
 
 const createUser = async (newUser) => {
-    try
-    {
-        let userToInsert = new userModel(newUser);
-        const createduser = await userToInsert.save( );
-        return createduser;
-    }
-    catch (error)
-    {
-        throw error;
-    }
+  try {
+    const userToInsert = new userModel(newUser);
+    const createdUser = await userToInsert.save();
+    return createdUser;
+  } catch (error) {
+    throw error;
+  }
 };
 
-    //UNCOMMENT AND REPLACE WHEN MIDDLEWARE IS IMPLEMENTED
-    // const updateUser = async(res.locals.userEmail, changes) => {
-    const updateUser = async(userEmail, changes) => {
-    try
-    {
-        //UNCOMMENT AND REPLACE WHEN MIDDLEWARE IS IMPLEMENTED
-        // let updatedUser = await userModel.findByOnedAndUpdate(res.locals.userEmail,
-        let updatedUser = await userModel.findByOneAndUpdate(userEmail,
-{ $set: changes }, { new: true });
-        return updatedUser;
-    }
-    catch (error)
-    {
-        throw error;
-    }
+const updateUser = async (userEmail, changes) => {
+  try {
+    const updatedUser = await userModel.findOneAndUpdate(
+      { userEmail },{ $set: changes },{ new: true }         
+    );
+    return updatedUser;
+  } catch (error) {
+    throw error;
+  }
 };
 
 module.exports = {
-    getUser,
-    createUser,
-    updateUser
-}
+  getUser,
+  createUser,
+  updateUser
+};
