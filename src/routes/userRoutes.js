0cbb719c10e18c2adff4d90917863d17c14bdfce
EnekoAzkue <eneko.azkue@ikasle.aeg.eus>
2025-09-28 23:27:52 +0200

    feat(CEJ-22) define route for log in and tests

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,10 +4,28 @@ const router = express.Router();
 const middleware = require("../middlewares/verifyData");
 const userController = require("../controllers/userController");
 
+// router.post(
+//   "/access-logged-in",
+//   middleware.verifyIdToken,
+//   userController.updateUser
+// );
+
+router.get(
+  "/kaotika",
+  middleware.verifyIdToken,       
+  userController.getKaotikaUser
+);
+
+router.get(
+  "/mongo",
+  middleware.verifyIdToken,       
+  userController.getMongoUser
+);
+
 router.post(
-  "/access-logged-in",
-  middleware.verifyIdToken,
-  userController.updateUser
+  "/login",
+  middleware.verifyIdToken,       
+  userController.loginUser
 );
 
 module.exports = router;
