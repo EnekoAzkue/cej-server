c3b7417e6de41dece3180018234ec801c19242cd
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2025-09-29 13:18:35 +0200

    fix(cej-39): get a Firebase-compatible ID token based on the Google's sent by the client

--- src/middlewares/verifyData.js
+++ src/middlewares/verifyData.js
@@ -1,10 +1,24 @@
 const { getAuth } = require("firebase-admin/auth");
 
-function verifyIdToken(req, res, next) {
+async function verifyIdToken(req, res, next) {
   const { idToken } = req.body;
 
+  const response = await fetch(
+    "https://identitytoolkit.googleapis.com/v1/accounts:signInWithIdp?key=AIzaSyAfZZEegCYwyzyN-8pCKbs7uBrSU8J7010",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        requestUri: `https://example.com${req.originalUrl}` /* TODO: Replace "example.com" with a valid value */,
+        postBody: `id_token=${idToken}&providerId=google.com`,
+        returnSecureToken: true,
+      }),
+    }
+  );
+
+  const { idToken: firebaseIdToken } = await response.json();
+
   getAuth()
-    .verifyIdToken(idToken)
+    .verifyIdToken(firebaseIdToken)
     .then((decodedToken) => {
       const userEmail = decodedToken.email;
       res.locals.userEmail = userEmail;
