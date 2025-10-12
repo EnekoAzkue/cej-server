85cb5e6f14bce97b33fcaeaa0f66d05ca7d40a1b
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2025-10-12 12:20:15 +0200

    feat(cej-59): event handler for the "access to/exit from lab" event

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,11 @@
 enum SocketServerToClientEvents {
+  ACOLYTE_INSIDE_OUTSIDE_LAB = "acolyte inside/outside lab",
   ACOLYTE_DISCONNECTED = "acolyte disconnected",
 }
 
 enum SocketClientToServerEvents {
   CONNECTION_OPEN = "connection open",
+  ACCESS_TO_EXIT_FROM_LAB = "access to/exit from lab",
 }
 
 export { SocketServerToClientEvents, SocketClientToServerEvents };
